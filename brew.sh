a39f50bc24cded04f016067a1ad227d3b82c5f3e
Junv Zhao <wahyd4@gmail.com>
2016-03-13 18:21:35 +0800

    remove some useless configuration and add brew install htop and gitextras

--- brew.sh
+++ brew.sh
@@ -38,8 +38,8 @@ brew install wget --with-iri
 # Install RingoJS and Narwhal.
 # Note that the order in which these are installed is important;
 # see http://git.io/brew-narwhal-ringo.
-brew install ringojs
-brew install narwhal
+# brew install ringojs
+# brew install narwhal
 
 # Install more recent versions of some OS X tools.
 brew install vim --override-system-vi
@@ -101,5 +101,8 @@ brew install tree
 brew install webkit2png
 brew install zopfli
 
+# Add by junv
+brew install htop 
+brew install gitextras
 # Remove outdated versions from the cellar.
 brew cleanup
